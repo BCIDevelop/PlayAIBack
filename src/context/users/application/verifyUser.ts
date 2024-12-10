c898c40e6604aa11b018bdd2c8e8d862f7b7d4ae
Luis Lopez <luis.lopez@utec.edu.pe>
2024-12-09 20:33:58 -0500

    feat:  test

--- src/context/users/application/verifyUser.ts
+++ src/context/users/application/verifyUser.ts
@@ -5,8 +5,10 @@ import { NotFound } from "../../../shared/errors";
 import { Types } from "mongoose";
 
 export const verifyUserDB = async (repository:UserRepository,jwtAdapter:JWTAdapter,token:string)=>{
+    console.log(token)
     const {email} = jwtAdapter.verifyToken(token) as JwtPayload
     const record = await repository.getUserByEmail(email as string)
+    console.log(record)
     if(!record) throw new NotFound("User not found")
-    repository.updateUser(record._id as Types.ObjectId,{active_status:true})
+    await repository.updateUser(record._id as Types.ObjectId,{active_status:true})
 }
--- src/context/users/controller.ts
+++ src/context/users/controller.ts
@@ -54,6 +54,7 @@ class UserController{
         return res.status(204).json({})
     }
     async verifyUser(req:Request,res:Response){
+        console.log('legooooooooooooooooooooooooooo')
         console.log(req.body.token)
         await this.service.verifyUserDB(this.repository,this.jwt,req.body.token)
         return res.status(204).json({})
